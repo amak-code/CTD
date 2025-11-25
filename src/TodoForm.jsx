d8badd7af83319fa749f56f57ce4845b7be757d5
Aleksandra Makhrova <amakhro@gmail.com>
2025-11-25 11:04:37 -0800

    ToDoForm and ToDoList files were created

---
+++ src/TodoForm.jsx
@@ -0,0 +1,13 @@
+function TodoForm() {
+  return (
+    <>
+      <form action="">
+        <label htmlFor="toDoTitle">ToDo</label>
+        <input type="text" id="toDoTitle" />
+        <button type="submit">Add ToDo</button>
+      </form>
+    </>
+  );
+}
+
+export default TodoForm;
